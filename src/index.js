6e9238d4ac91457c3ecd8731f711f6b3b92246c3
kkpatel911 <kkpatel911@gmail.com>
2019-11-10 23:42:56 -0500

    added functionality for taking screenshots

---
+++ src/index.js
@@ -0,0 +1,39 @@
+// const path = require("path");
+const { desktopCapturer } = require("electron");
+const path = require("path");
+const fs = require("fs");
+
+// // Button that takes screenshot
+const screenShotButton = document.getElementById("notifyBtn");
+
+// console.log("Kishan");
+screenShotButton.addEventListener("click", function(e) {
+  let options = {
+    types: ["screen"],
+    thumbnailSize: {
+      width: screen.width,
+      height: screen.height
+    }
+  };
+
+  desktopCapturer.getSources(options, (err, sources) => {
+    if (err) {
+      return console.log(err);
+    }
+    sources.forEach(function(source) {
+      console.log(source);
+      if (source.name === "Entire Screen") {
+        const screenShotPath = path.join(
+          __dirname,
+          "../assets/images/screenshot.png"
+        );
+
+        fs.writeFile(screenShotPath, source.thumbnail.toPNG(), err => {
+          console.log(err);
+        });
+      }
+    });
+  });
+});
+
+console.log("Kishan");
